d90e0f9f43a7648cdddeaea88b90ccf410308bc2
ITPiMa <55231246+ITPiMa@users.noreply.github.com>
2019-09-13 15:49:41 +0200

    Adding bad jokes to bot

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // require the discord.js module
 const Discord = require('discord.js');
+var request = require('request');
+
 
 // create a new Discord client
 const client = new Discord.Client();
@@ -7,12 +9,21 @@ const client = new Discord.Client();
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+    
 });
 client.on('message', message => {
-	if (message.content === '!givemeameme') {
-        
-		message.channel.send('Screw you.');
+	if (message.content === '!dadjoke') {
+       const options = {
+            url: 'https://icanhazdadjoke.com/',
+            method: 'GET',
+            headers: {
+                'Accept': 'text/plain',
+                'Accept-Charset': 'utf-8'
+            }
+        };
+        request(options, function (error, response, body) {
+          message.channel.send(response.body); // Print the error if one occurred
+        });
 	}
 });
 // login to Discord with your app's token
